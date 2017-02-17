026056279dc3ccba782a3036398e46702b27f53c
peter <729245768@qq.com>
2017-02-17 15:23:33 +0800

    完善UserInfo类

--- src/com/student/model/UserInfo.java
+++ src/com/student/model/UserInfo.java
@@ -1,7 +1,31 @@
 package com.student.model;
 
-/**
+/**用户信息类
  * Created by peter on 2017/2/17.
  */
 public class UserInfo {
+    private String username;
+    private String password;
+
+    public UserInfo(){}
+
+    public UserInfo(String username,String password){
+        this.username = username;
+        this.password = password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
