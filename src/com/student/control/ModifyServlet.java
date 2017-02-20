f2c24676e43488457a5ae32ad9768ab7df49c9ab
peter <729245768@qq.com>
2017-02-20 10:38:12 +0800

    处理修改的跳转动作

---
+++ src/com/student/control/ModifyServlet.java
@@ -0,0 +1,33 @@
+package com.student.control;
+
+import com.student.model.StudentInfo;
+import com.student.service.StudentDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by peter on 2017/2/19.
+ */
+@WebServlet(name = "ModifyServlet")
+public class ModifyServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String studentID = request.getParameter("studentid");
+        StudentDao studentDao = new StudentDao();
+        StudentInfo studentInfo = studentDao.queryStudentInfo(studentID);
+        if(studentInfo!=null){
+            HttpSession session = request.getSession();
+            session.setAttribute("studentInfo",studentInfo);
+        }
+        response.sendRedirect("/modifyStudentInfoSingle.jsp");
+    }
+}
