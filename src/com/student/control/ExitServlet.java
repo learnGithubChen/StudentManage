df9f1349e677d8eaabcf139d957f0daa70c0811a
peter <729245768@qq.com>
2017-02-20 10:40:22 +0800

    相应用户退出操作

---
+++ src/com/student/control/ExitServlet.java
@@ -0,0 +1,25 @@
+package com.student.control;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by peter on 2017/2/17.
+ */
+@WebServlet(name = "ExitServlet")
+public class ExitServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("user");//删除session关于user信息
+        response.sendRedirect("/index.jsp");
+    }
+}
